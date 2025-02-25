52febfdb0027df2e54851e2c78c8b6aa2618cd2a
alexxxey <119540119+alexxxey0@users.noreply.github.com>
2025-02-25 23:52:15 +0200

    undo commit

--- frontend/CSE470-PROJECT/template/header.php
+++ frontend/CSE470-PROJECT/template/header.php
@@ -15,7 +15,6 @@ if (isset($_SESSION['email'])) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="google-site-verification" content="uiajK5ptFjleXcK1zWZ_zN7SWiD3SaS46WbfLfwf9S4" />
     <title><?php echo isset($title) ? $title : 'Online Bookshop'; ?></title>
     <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="./bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
